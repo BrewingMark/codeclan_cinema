c9002445a4c2b54d35f3069b708a716a69fd3a86
Mark Nicol <marknicol1991@live.co.uk>
2020-03-27 19:12:09 +0000

    Adds update and delete methods for ticket class.

--- console.rb
+++ console.rb
@@ -19,13 +19,13 @@ film2 = Film.new({'title' => 'Into The Wild', 'price' => 10})
 film2.save()
 
 ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
-# ticket1.save()
+ticket1.save()
 ticket2 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
-# ticket2.save()
+ticket2.save()
 ticket3 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
-# ticket3.save()
+ticket3.save()
 ticket4 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film2.id})
-# ticket4.save()
+ticket4.save()
 
 binding.pry
 nil
